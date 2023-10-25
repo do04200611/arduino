79717e6e79d31e1d6d67bfe7a9bf5ca64959c03d
kimganghyeon <kimganghyeon6080@naver.com>
2023-10-25 17:42:27 +0900

    Update publicdatapiezobuzoproject.ino

--- publicdatapiezobuzoproject/publicdatapiezobuzoproject.ino
+++ publicdatapiezobuzoproject/publicdatapiezobuzoproject.ino
@@ -1,9 +1,12 @@
 #include <ESP8266WiFi.h>
 
-const char *ssid = "DIT_FREE_WiFi";
+const char *ssid = "DIT_CS_705";
 const char *password = "";
 #define piezzo D1
+#define RL D4
+#define YL D5
 int piezzostate = LOW;
+int ledState = LOW;
 
 WiFiServer server(80);
 WiFiClient client;
@@ -11,6 +14,8 @@ WiFiClient client;
 void setup() {
   Serial.begin(115200);
   pinMode(piezzo, OUTPUT);
+  pinMode(RL, OUTPUT);
+  pinMode(YL, OUTPUT);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -29,7 +34,7 @@ void setup() {
 void loop() {
   client = server.available();
   if (!client) return;
-
+=
   Serial.println("new client");
   while (!client.available()) {
     delay(1);
@@ -47,15 +52,34 @@ void loop() {
     digitalWrite(piezzo, LOW);
     piezzostate = LOW;
   }
-
+    if(request.indexOf("ON1")!=-1){
+    digitalWrite(RL, HIGH);
+    ledState=HIGH;
+  }
+  if(request.indexOf("OFF1")!=-1){
+    digitalWrite(RL, LOW);
+    ledState=LOW;
+  }
+     if(request.indexOf("ON2")!=-1){
+    digitalWrite(YL, HIGH);
+    ledState=HIGH;
+  }
+  if(request.indexOf("OFF2")!=-1){
+    digitalWrite(YL, LOW);
+    ledState=LOW;
+  }
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html; charset=UTF-8");
   client.println("");
 
-  String response = "<!DOCTYPE HTML><HTML><br>";
-  response += "Click <a href=\"/piezzoOff\">here</a> 버튼을 누르세요<br>";
-  response += "Click <a href=\"/piezzoOn\">here</a> 버튼을 누르세요<br>";
-  response += "</HTML>";
+  String aa = "<!DOCTYPE HTML><HTML><br>";
+  aa += "<a href=\"piezzoOff\">킬 때</a>  버튼을 누르세요<br>";
+  aa += "<a href=\"piezzoOn\">끌 때</a>버튼을 누르세요<br>";
+  aa += "<a href=\"ON1\">킬 때</a> RL<br>";
+  aa += " <a href=\"OFF1\">끌 때</a> RL<br>";
+  aa += "<a href=\"ON2\">킬 때\">YL</a><br>";
+  aa += "<a href=\"OFF2\">끌 때\"></a>YL<br>";
+  aa += "</HTML>";
 
-  client.println(response);
+  client.println(aa);
 }
